03c12556f3224ebcfa3bd8b2d70f91a30a433ecd
bena33 <92751882+bena33@users.noreply.github.com>
2024-03-25 20:35:10 +0100

    prueba

--- app/src/main/java/projectofinal/alternativedex/activities/DetailActivity.java
+++ app/src/main/java/projectofinal/alternativedex/activities/DetailActivity.java
@@ -47,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         setupToolbar();
-
+        //kgghjhjhgjhg
         Intent intent = getIntent();
         if (intent != null) {
             String pokemonName = intent.getStringExtra("name");
